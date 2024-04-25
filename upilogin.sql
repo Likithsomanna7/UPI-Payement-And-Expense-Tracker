00feb5470caea410cc5348c82d7820b9044db7e5
Krishnaidnani <krishnaidnani7@gmail.com>
2024-04-25 15:16:16 +0530

    added bankinfo database

--- upilogin.sql
+++ upilogin.sql
@@ -11,6 +11,14 @@ PRIMARY KEY(User_id)
 );
 ALTER TABLE Login
 DROP CONSTRAINT CK_Login_PhoneNumber;
+CREATE TABLE bankinfo(
+User_id int NOT NULL,
+BanKName varchar(50) NOT NULL,
+Account_No char(11) NOT NULL PRIMARY KEY ,
+Account_Type varchar(20),
+AccountBalance bigint, 
+FOREIGN KEY (User_id) REFERENCES Login(User_id)
+);
 show tables;
 desc Login;
 select * from Login;
